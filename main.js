f912070ab2fff6653e7d93b4ae9098eed120a3f0
yasmin-max <yasimkim007@gmail.com>
2024-12-26 22:15:27 +0300

    phones

---
+++ main.js
@@ -0,0 +1,16 @@
+let img = document.querySelector('.img');
+let container =document.querySelector('.container')
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.background=color;
+}
+/*let img = document.querySelector('.img');
+let container =document.querySelector('.container')
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.background =color;
+}*/
